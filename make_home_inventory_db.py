10209f6334ae097f1f3a53e929803325372e90ea
Scott McMahan <scottmcmahan@scotts-mbp.attlocal.net>
2020-02-29 18:56:03 -0600

    Added new version of db making file.

--- make_home_inventory_db.py
+++ make_home_inventory_db.py
@@ -9,18 +9,41 @@ USER = "root"
 PASSWORD = "flPyog?nL4Ww"
 HOST = "127.0.0.1"
 PORT = "3306"
-DATABASE = "home_inventory_db"
-TABLENAME = "order_products_prior"
-# create the dataframe
-final_df=pd.read_csv("order_products__prior.csv")
+DATABASE = "the_home_inventory"
+
+
+
+
+
+#create the dataframe
+
 engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}")
-# try:
-#     engine.execute(f"CREATE DATABASE {DATABASE}")
-# except ProgrammingError:
-#     warnings.warn(
-#         f"Could not create database {DATABASE}. Database {DATABASE} may already exist."
-#     )
-#     pass
+try:
+    engine.execute(f"CREATE DATABASE {DATABASE}")
+except ProgrammingError:
+    warnings.warn(
+        f"Could not create database {DATABASE}. Database {DATABASE} may already exist."
+    )
+    pass
 engine.execute(f"USE {DATABASE}")
-engine.execute(f"DROP TABLE IF EXISTS {TABLENAME}")
-final_df.to_sql(name=TABLENAME, con=engine)
+tables_list=["order_products_prior", "order_products_train", "orders", "products"]
+csv_list=["order_products__prior.csv","order_products__train.csv", "orders.csv", "products.csv"]
+
+def make_df:
+    df=pd.read_csv(csv)
+    return df
+
+def make_table(TABLENAME):
+    engine.execute(f"DROP TABLE IF EXISTS {TABLENAME}")
+    df.to_sql(name=TABLENAME, con=engine)
+    return
+
+for csv in csv_list:
+    for TABLENAME in tables_list:
+        make_df(csv)
+        make_table(TABLENAME)
+
+
+
+
+    
