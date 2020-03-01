5aa13d89985d8c8d4f830a127fe3faa4b824cef0
Scott McMahan <scottmcmahan@scotts-mbp.attlocal.net>
2020-02-29 19:42:46 -0600

    Added make_home_inventory_db.py

--- make_home_inventory_db.py
+++ make_home_inventory_db.py
@@ -2,14 +2,15 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 from sqlalchemy.exc import ProgrammingError
-
+import os
+import warnings
 import pymysql
 
 USER = "root"
 PASSWORD = "flPyog?nL4Ww"
 HOST = "127.0.0.1"
 PORT = "3306"
-DATABASE = "the_home_inventory"
+DATABASE = "home_inventory"
 
 
 
@@ -29,21 +30,23 @@ engine.execute(f"USE {DATABASE}")
 tables_list=["order_products_prior", "order_products_train", "orders", "products"]
 csv_list=["order_products__prior.csv","order_products__train.csv", "orders.csv", "products.csv"]
 
-def make_df:
-    df=pd.read_csv(csv)
+def make_df(csv):
+    this_csv=os.path.join("resources", csv)
+    df=pd.read_csv(this_csv)
     return df
 
-def make_table(TABLENAME):
+def make_table(TABLENAME, df):
     engine.execute(f"DROP TABLE IF EXISTS {TABLENAME}")
     df.to_sql(name=TABLENAME, con=engine)
     return
 
 for csv in csv_list:
     for TABLENAME in tables_list:
-        make_df(csv)
-        make_table(TABLENAME)
+        
+        df=make_df(csv)
+        make_table(TABLENAME, df)
 
-
+'''This creates four tables and the home_inventory database. In SQLWorkbench order_products_train is added to order_products_prior.'''
 
 
     
