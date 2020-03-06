159240098379ee04815d34f66ced2cf5ace01fd7
Scott McMahan <scottmcmahan@scotts-mbp.attlocal.net>
2020-03-06 12:25:47 -0600

    Added beginnings of DBconnection.py

---
+++ DBconnection.py
@@ -0,0 +1,22 @@
+#Starter Inventory App
+import numpy as np
+import pandas as pd
+import datetime as dt
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy import inspect, desc
+import os
+from pprint import pprint
+
+class SQLalchemy_connection():
+    def db_connect(self):
+        return
+    def db_query(self):
+        return
+
+    def create_json(self):
+        return
+
+    
