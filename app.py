152caea0dff1572a56b9b790f53de6397b4966dc
techcheese <cesartole73@gmail.com>
2020-03-18 04:00:30 -0500

    added flask extensions, connected database

--- app.py
+++ app.py
@@ -1,31 +1,61 @@
 from flask import Flask, request, render_template, redirect
-from flask_bootstrap import flask_bootstrap
-from flask_wtf import Flaskform
-from wtf
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from app_config import username, password, server, database
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+#from wtf
 import datetime
 
 app = Flask(__name__)
+Bootstrap(app)
+
+db_uri = f"mysql://{username}:{password}@{server}/{database}"
+app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+
+db = SQLAlchemy(app)
 
 #place in a different file
 app.secret_key = 'myprecious'
-class LoginForm(Flaskform):
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
 
-class RegisterForm(Flaskform):
-    email = StringField('email', validator=[InputRequired90, Email(message="Invalid Email"), Length=(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message="Invalid Email")])
+    username = StringField('username', validators=[InputRequired(), Length(min=4,  max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 @app.route('/')
+def land():
+    return render_template('login.html')
 
 @app.route('/index.html')
 
-@app.route('/home.html')
+@app.route('/login.html')
+def login():
+    form = LoginForm()
+
+    return render_template('login.html', )
 
 @app.route('/signup.html')
+def signup():
+    form = RegisterForm()
+
+    return render_template('signup.html', form=form)
 
 @app.route('/dashboard.html')
+def some():
+    return None
 
 app.run(debug=True)
