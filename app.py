4b9953dc865b11ee3f9947db965174dbdf9e311c
techcheese <cesartole73@gmail.com>
2020-03-21 00:11:38 -0500

    connect local database, checks hash passwords

--- app.py
+++ app.py
@@ -1,36 +1,37 @@
-from flask import Flask, request, render_template, redirect, render_template_string
+from flask import Flask, request, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import datetime
-from app_config import username, password, server, database
+from werkzeug.security import generate_password_hash, check_password_hash
+#from app_config import username, password, server, database
+from testconfig import DIALECT, DRIVER, USERNAME, PASSWORD, DATABASE, HOSTNAME, PORT
+db_uri = f"{DIALECT}+{DRIVER}://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}"
+
 
 
 app = Flask(__name__)
-Bootstrap(app)
-
-db_uri = f"mysql+mysqlconnector://{username}:{password}@{server}/{database}"
-app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
-
-
-
-db = SQLAlchemy(app)
-
-#place in a different file
 app.secret_key = 'myprecious'
+app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+Bootstrap(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
+    passw = db.Column(db.String(80))
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -44,9 +45,7 @@ class RegisterForm(FlaskForm):
 
 @app.route('/')
 def home_page():
-    # String-based templates
     return render_template('base.html')
-#@app.route('/index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -55,11 +54,11 @@ def login():
     if form.validate_on_submit():
 
         user = User.query.filter_by(username=form.username.data).first()
-
         if user:
-            if user.password == form.password.data:
+            if check_password_hash(user.passw, form.password.data):
+                login_user(user, remember=form.remember.data)
                 return redirect('/dashboard')
-        
+            return '<h1> Invalid username or password. </h1>'
 
     return render_template('login.html', form=form)
 
@@ -67,17 +66,20 @@ def login():
 def signup():
     form = RegisterForm()
 
+
     if form.validate_on_submit():
-        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(id=None, username=form.username.data, email=form.email.data, passw=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        return '<h1>New user has been created</h1>'
+        return '<h2>New user has been created, please log in.</h2>'
 
 
     return render_template('signup.html', form=form)
 
-@app.route('/dashboard.html')
-def some():
-    return None
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 app.run(debug=True)
---
+++ testconfig.py
@@ -0,0 +1,8 @@
+HOSTNAME = "127.0.0.1"
+PORT = 3306
+USERNAME = "root"
+PASSWORD = "UTboot1517!"
+DIALECT = "mysql"
+DRIVER = "pymysql"
+DATABASE = "inventory_test"
+
