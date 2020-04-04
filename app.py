5a8faa50aae929028d8745364262cf530f9cd841
Derrick <derrickhbutler@gmail.com>
2020-04-03 21:41:24 -0400

    config changes

--- app.py
+++ app.py
@@ -22,10 +22,9 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import scoped_session
 from werkzeug.security import generate_password_hash, check_password_hash
-from app_config import secret, HOST, PORT, DATABASE, DIALECT, DRIVER
-from memet_config import USER, PASSWORD
+from app_config import secret, HOST, PORT, DATABASE, DIALECT, DRIVER, USER, PASSWORD
 from database import SessionLocal, engine, Base, SQALCHEMY_DATABASE_URL
-from models import DictMixIn, RegisterForm, LoginForm, Orders, Products, Departments, Aisles, Order_products
+from models import DictMixIn, RegisterForm, LoginForm, Orders, Products, Departments, Aisles, Order_products, Order_products_prior
 import datetime
 from flask_sqlalchemy import SQLAlchemy
 
--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app_config import HOST, PORT, DATABASE, DIALECT, DRIVER, secret
-from memet_config import USER, PASSWORD 
+from app_config import USER, PASSWORD,  HOST, PORT, DATABASE, DIALECT, DRIVER, secret
+ 
 
 
-SQALCHEMY_DATABASE_URL = f"{DIALECT}+{DRIVER}://{USER}:{PASSWORD}@{HOST}/local_inventory"
+SQALCHEMY_DATABASE_URL = f"{DIALECT}+{DRIVER}://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
 
 engine = create_engine(
     SQALCHEMY_DATABASE_URL
