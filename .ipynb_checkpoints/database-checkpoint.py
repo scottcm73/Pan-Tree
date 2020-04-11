c8bfb70a98de47ed6ab465e36e999f9d29277247
derrickhbutler <55453470+derrickhbutler@users.noreply.github.com>
2020-04-10 21:24:38 -0500

    nutrient data dicts by order

--- .ipynb_checkpoints/database-checkpoint.py
+++ .ipynb_checkpoints/database-checkpoint.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app_config import USER, PASSWORD,  HOST, PORT, DATABASE, DIALECT, DRIVER, secret
  
+
+
 SQALCHEMY_DATABASE_URL = f"{DIALECT}+{DRIVER}://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
 
 engine = create_engine(
