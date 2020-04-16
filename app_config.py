1bd8d97dcd388acee6c47b7a52c2928b955141e7
Scottdude73 <53838044+scottcm73@users.noreply.github.com>
2020-04-16 11:57:29 -0500

    changed password

--- app_config.py
+++ app_config.py
@@ -3,7 +3,7 @@
 DIALECT = "mysql"
 DRIVER = "pymysql"
 USER = 'datascm2_web'
-PASSWORD =  'kTTNMG82ujfQoWJ5M7mb614L0lSMvKmXf7Mim0s3hSoyJxyqLYdzBr6TZ025hzEp'
+PASSWORD =  'Fk45QbHgy8GEMLQX8FAy5SHA2evXYwbMbd43xmzsJm92RE5GNDkRP7mQKPg2YDpA'
 HOST = '162.241.193.35'
 PORT='3306'
 DATABASE = 'datascm2_home_inventory_db'
