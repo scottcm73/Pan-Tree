4ec2f8bf01ab142629a49570fe49c6bcc417eec6
Scottdude73 <53838044+scottcm73@users.noreply.github.com>
2020-04-30 19:32:12 -0500

    Added in_search3.py

---
+++ in_search3.py
@@ -0,0 +1,76 @@
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import Normalizer
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.datasets import dump_svmlight_file
+from stopwds import stopwords
+import pandas as pd 
+import os
+import pickle
+import re
+
+def clean_product_data(df):
+    
+    products_df = df
+  
+
+    
+    products_df['product_name']=products_df['product_name'].map(lambda x: re.sub(r'\W+', ' ', x))
+
+    # Replace hyphens with spaces
+
+    products_df['product_name']=products_df['product_name'].map(lambda x: re.sub(r'/-/g', ' ', x ))
+    # Strip away numbers
+    products_df['product_name']=products_df['product_name'].map(lambda x: re.sub(r'\d+', '', x ))
+   
+    
+    products_df['product_name'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stopwords)]))
+    print(products_df.head())
+    
+    return products_df
+products_wm_path = os.path.join('Resources', "t_o_pro_join_pro.csv")
+df = pd.read_csv(products_wm_path, index_col=False)
+df2 = clean_product_data(df)
+npdf2=df2.to_numpy()
+print(npdf2)
+X_text = df2["product_name"].values
+tfidf = TfidfVectorizer(ngram_range=(3, 7),
+            analyzer="char")
+
+
+tfidf.fit(X_text)
+X = tfidf.transform(X_text)
+
+file_name2 = os.path.join("Resources", "products_np.pkl")
+with open(file_name2, 'wb') as f:
+    this_np=pickle.dump(npdf2, f)
+
+
+def search(term):
+    K=5
+
+ 
+
+    
+
+    X_term = tfidf.transform([term])
+    simularities = cosine_similarity(X_term, X)
+    idxmax = np.argpartition(-simularities, K)  
+    return idxmax
+
+if __name__ == "__main__":
+    term = "choclate cooies mnt"
+    file_name2 = os.path.join("Resources", "products_np.pkl")
+    with open(file_name2, 'rb') as f:
+        this_np=pickle.load(f)
+
+    print(this_np)
+    idx=search(term)
+    print(idx)
+
+    idxs=idx[0]
+    for x in range(5):
+        print(this_np[idxs[x]])
+
+   
