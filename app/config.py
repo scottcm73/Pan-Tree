42caac9a774e94db7d1645c6505e1f55415f0e8c
Memet Bulut <55109910+mtbulut@users.noreply.github.com>
2020-05-02 03:29:09 -0500

    deleted

--- app/config.py
+++ app/config.py
@@ -16,7 +16,7 @@ from secret import PASSWORD, KEY
 DIALECT = "mysql"
 DRIVER = "pymysql"
 USER = "datascm2_web"
-
+PASSWORD =  'Fk45QbHgy8GEMLQX8FAy5SHA2evXYwbMbd43xmzsJm92RE5GNDkRP7mQKPg2YDpA'
 HOST = "35.232.35.9"
 PORT = "3306"
 DATABASE = "datascm2_home_inventory_db"
