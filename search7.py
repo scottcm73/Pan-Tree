2001dfff5e5a2625a6c7dc572e23e35a59ac9b9c
Scott McMahan <scottcmcmahan@gmail.com>
2020-05-10 15:28:14 -0500

    adding all files

--- search7.py
+++ search7.py
@@ -5,39 +5,34 @@ from sklearn.pipeline import make_pipeline
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.datasets import dump_svmlight_file
 from stopwds import stopwords
-import pandas as pd 
+import pandas as pd
 import os
 import pickle
 import joblib
 
 
-
-
-
-
 def search(term):
-    K=5
-    file_name1 = os.path.join("Resources", "tfidf_pro.joblib")
-    tfidf=joblib.load(file_name1)
-    file_name3 = os.path.join("Resources", "transformed_matrix_pro.pkl")
-    with open(file_name3, 'rb') as f:
+    K = 5
+    file_name1 = os.path.join("..", "Resources", "tfidf_pro.joblib")
+    tfidf = joblib.load(file_name1)
+    file_name3 = os.path.join("..", "Resources", "transformed_matrix_pro.pkl")
+    with open(file_name3, "rb") as f:
         X = pickle.load(f)
-    
+
     X_term = tfidf.transform([term])
     simularities = cosine_similarity(X_term, X)
-    idxmax = np.argpartition(-simularities, K)  
+    idxmax = np.argpartition(-simularities, K)
     return idxmax
+
 
 if __name__ == "__main__":
     term = "choclate cooies mnt"
-    file_name2 = os.path.join("Resources", "products_np_pro.pkl")
-    with open(file_name2, 'rb') as f:
-        this_np=pickle.load(f)
+    file_name2 = os.path.join("..", "Resources", "products_np_pro.pkl")
+    with open(file_name2, "rb") as f:
+        this_np = pickle.load(f)
 
-    print(this_np)
-    idx=search(term)
-    print(idx)
+    idx = search(term)
 
-    idxs=idx[0]
+    idxs = idx[0]
     for x in range(5):
         print(this_np[idxs[x]])
