8be853e966c08923961984482afd4a5bc4ebafa4
Scottdude73 <53838044+scottcm73@users.noreply.github.com>
2020-05-10 15:49:44 -0500

    eliminated secret.py reference
    For heroku

--- config.py
+++ config.py
@@ -10,7 +10,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy import SQLAlchemy
-from secret import PASSWORD, KEY
 
 
 DIALECT = "mysql"
