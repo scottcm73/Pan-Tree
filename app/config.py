22977bfd7fc737f34195da32cba77fab07abe59d
Scottdude73 <53838044+scottcm73@users.noreply.github.com>
2020-05-12 20:58:05 -0500

    Updated config.py
    Fixed error in syntax

--- app/config.py
+++ app/config.py
@@ -10,33 +10,26 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy import SQLAlchemy
-import os
-
-PASSWORD = os.environ.get('PASSWORD')
-KEY = os.environ.get('KEY')
-
-
-from secret import KEY, PASSWORD
-#PASSWORD = os.environ.get('PASSWORD')
-#KEY = os.environ.get('KEY')
+from secret import PASSWORD, KEY
 
 
 DIALECT = "mysql"
 DRIVER = "pymysql"
 USER = "datascm2_web"
 
-HOST = "162.241.193.35"
+HOST = "35.232.35.9"
 PORT = "3306"
 DATABASE = "datascm2_home_inventory_db"
 
-SQALCHEMY_DATABASE_URL = (f"{DIALECT}+{DRIVER}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+sqstring = f'{DIALECT}+{DRIVER}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
+SQALCHEMY_DATABASE_URL = (sqstring)
 
 
 ##database configuration
 engine = create_engine(SQALCHEMY_DATABASE_URL, pool_recycle=3600, pool_pre_ping=True)
 
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 ## needed for User methods with flask_login
