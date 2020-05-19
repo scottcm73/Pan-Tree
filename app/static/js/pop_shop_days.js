b2638e35e3f25917d19a5323bbc265f5d635f713
Thomas Gomez <thomasg@indeed.com>
2020-05-19 08:14:36 -0500

    adding most popular shopping days csv and js, html udpates

---
+++ app/static/js/pop_shop_days.js
@@ -0,0 +1,30 @@
+function makeplot() {
+    Plotly.d3.csv(".../Resources/pop_shop_days.csv", function (data) { processData(data) });
+
+};
+
+function processData(allRows) {
+
+    console.log(allRows);
+    var x = [], y = [];
+
+    for (var i = 0; i < allRows.length; i++) {
+        row = allRows[i];
+        x.push(row['order_date']);
+        y.push(row['weekday']);
+    }
+    console.log('X', x, 'Y', y);
+    makePlotly(x, y);
+}
+
+function makePlotly(x, y) {
+    var plotDiv = document.getElementById("plot");
+    var traces = [{
+        x: x,
+        y: y
+    }];
+
+    Plotly.newPlot('pop_shop_days', traces,
+        { title: 'Most Popular Shopping Days' });
+};
+makeplot();
